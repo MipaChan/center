7c78ace8060da6419563c01bd528309a8f4d5f79
mipa <vrilcc@163.com>
2023-06-14 04:28:45 +0800

    update

---
+++ functions/api/test.ts
@@ -0,0 +1,25 @@
+import { Client } from "pg";
+
+export interface Env {
+    // This should be a valid Postgres connection string
+    // For example, "postgres://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs"
+    // Use `wrangler secret put DB_URL` to configure a Secret with your connection string
+    DB_URL: string;
+}
+
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+    const client = new Client({
+        user: 'postgres',
+        password: 'Slwnzq520..',
+        host: '120.27.143.135',
+        port: 15432,
+        database: 'fgo'
+    })
+    await client.connect()
+
+    const result = await client.query({
+        text: "SELECT * FROM configs LIMIT 10",
+    });
+    return new Response('Creating Todo: ' + JSON.stringify(result.rows));
+}
